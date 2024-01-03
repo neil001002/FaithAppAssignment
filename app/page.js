c61f6ec12dba1b93a53ffc9a9036b5a08f7253ac
sunil <neil001002@gmail.com>
2024-01-03 21:45:09 +0530

    first commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,188 @@
-import Image from 'next/image'
+// "use client";
+// import Image from "next/image";
 
-export default function Home() {
+// import { useEffect, useState } from "react";
+import { promises as fs } from "fs";
+
+export default async function Home() {
+  const file = await fs.readFile(process.cwd() + "/app/data.json", "utf8");
+  const data = JSON.parse(file);
+
+  console.log(file);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="">
+      <section className="relative h-[90vh] flex items-end">
+        {/* Background Image */}
+        <div
+          className="absolute inset-0 bg-cover z-0"
+          // style={{ backgroundImage: 'url("/NITYANAND CHARAN DAS.jpg")' }}
+          style={{
+            backgroundImage:
+              'linear-gradient(to right, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0)), url("/NITYANAND CHARAN DAS.jpg")',
+          }}
+        ></div>
+
+        {/* Overlay for Image */}
+        {/* <div className="absolute inset-0 bg-black opacity-50 z-10 "></div> */}
+
+        {/* Content */}
+        <div className="container mx-auto z-20 text-left mb-8 px-20">
+          <h1 className="text-xl lg:text-3xl text-white mb-4">
+            {data.instructor.name}
+          </h1>
+          <p className="text-4xl lg:text-6xl font-semibold text-white leading-tight w-[60vw]">
+            {data.course.title}
+          </p>
         </div>
-      </div>
+      </section>
+      <section className="lg:px-20 w-[60vw]">
+        <ul className="flex lg:gap-4 text-2xl">
+          <li className="border-b-4 p-4 border-purple-800">ABOUT</li>
+          <li className="p-4">INSTRUCTOR</li>
+          <li className="p-4">REVIEWS</li>
+        </ul>
+      </section>
+      <section className="lg:px-20 mt-10 w-[60vw]">
+        <h1 className="text-4xl lg:text-5xl font-bold">
+          About the course
+        </h1>
+        <p className="mt-10 text-2xl leading-normal ">
+          This course distills wisdom from the Bhagavad Gita into four
+          major themes. It covers mastering thoughts and emotions for inner
+          peace (Mind Control), setting realistic goals and maintaining
+          life balance (Managing Expectations), resolving conflicts with
+          empathy and ethical principles (Conflict Resolution), and
+          managing negative feelings while cultivating positivity
+          (Controlling Negative Emotions). The course includes interactive
+          sessions, reflective exercises, and discussions, making the
+          Gita's teachings applicable to contemporary life.
+        </p>
+        <h1 className="text-4xl mt-10 lg:text-5xl font-bold">
+          What to expect from the course
+        </h1>
+        <ol className="mt-10 text-2xl leading-normal ml-10">
+          <li style={{ listStyleType: "square" }}>
+            Learn to manage your relationships{" "}
+          </li>
+          <li style={{ listStyleType: "square" }}>Better communication</li>
+          <li style={{ listStyleType: "square" }}>Time management</li>
+          <li style={{ listStyleType: "square" }}>Forgiveness</li>
+        </ol>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+        <h1 className="text-4xl mt-10 lg:text-5xl font-bold">
+          Key topics covered
+        </h1>
+        <p className="mt-10 text-2xl leading-normal ">
+          Learn Key Life lessons from the Gita: Mind Control, Managing
+          Expectations, Conflict Resolution and Controlling Negative
+          Emotions
+          <ul className="ml-10">
+            <li style={{ listStyleType: "square" }}>
+              <span className="font-bold">
+                10 written and audio sessions
+              </span>{" "}
+              guide you in decoding the language of numbers so that you can
+              easily receive their signs and messages.
+            </li>
+            <li style={{ listStyleType: "square" }}>
+              <span className="font-bold">
+                Intuitive exersises and homework
+              </span>{" "}
+              walk you through the energies and and values of numbers and
+              number sequences so you can become your own authority.
+            </li>
+            <li style={{ listStyleType: "square" }}>
+              <span className="font-bold">A handy reference</span> to
+              numeric meanings so you can uncover your soul's gifts,
+              challenges, lessons, and purpose - and receive spiritual
+              guidance along your path.
+            </li>
+            <li style={{ listStyleType: "square" }}>
+              <span className="font-bold">Expert techniques</span> for
+              manifestation offer simple ways to use numbers to co-create
+              with the universe.
+            </li>
+          </ul>
+        </p>
+      </section>
+      <section className="lg:px-20 mt-10 flex flex-col gap-10">
+        <h1 className="text-4xl lg:text-5xl font-bold">
+          About the Instructor
+        </h1>
+        <div className="lg:flex gap-4">
+          <img
+            className="rounded-full h-60"
+            src="/NITYANAND CHARAN DAS_profile_pic.jpg"
+          />
+          <div>
+            <p className="text-2xl">
+              Lorem ipsum text about the instructor's background and
+              philosophy. Lorem ipsum text about the instructor's
+              background and philosophy. Lorem ipsum text about the
+              instructor's background and philosophy.
+            </p>
+          </div>
+          <div>
+            <p className="text-2xl">
+              Lorem ipsum text about the instructor's background and
+              philosophy. Lorem ipsum text about the instructor's
+              background and philosophy. Lorem ipsum text about the
+              instructor's background and philosophy.
+            </p>
+          </div>
+        </div>
+        <div className="flex justify-around">
+          <div className="flex gap-2">
+            <img src="/facebook.svg" />
+            <span>Facebook</span>
+          </div>
+          <div className="flex gap-2">
+            <img src="/twitter.svg" />
+            <span>Facebook</span>
+          </div>
+          <div className="flex gap-2">
+            <img src="/facebook.svg" />
+            <span>Facebook</span>
+          </div>
+          <div className="flex gap-2">
+            <img src="/instagram.svg" />
+            <span>Facebook</span>
+          </div>
+        </div>
+      </section>
+      <section className="relative mt-10 bg-slate-300 flex flex-col gap-8 p-10 items-center justify-center">
+        {" "}
+        <div className="flex items-center justify-center px-48 text-center ">
+          <p className="text-4xl font-bold">{data.testimonial.text}</p>
+        </div>
+        <div className="flex gap-4 items-center">
+          <img
+            className="h-12 rounded-full"
+            src="/NITYANAND CHARAN DAS_profile_pic.jpg"
+          />
+          <div className="flex flex-col">
+            <span className="font-semibold">
+              {data.testimonial.reviewer_name}
             </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+            <span>{data.testimonial.reviewer_designation}</span>
+          </div>
+        </div>
+        <div className="flex justify-center items-center gap-1">
+          <span className="bg-slate-500 rounded-full h-2 w-2"></span>
+          <span className="bg-slate-500 rounded-full h-2 w-2"></span>
+          <span className="bg-slate-500 rounded-full h-2 w-6"></span>
+        </div>
+        <div className="absolute flex justify-between w-full px-20">
+          <span className="h-10 w-10 flex items-center justify-center rounded-full bg-slate-400">
+            {"<"}
+          </span>
+          <span className="h-10 w-10 flex items-center justify-center rounded-full bg-slate-400">
+            {">"}
+          </span>
+        </div>
+      </section>
+      {/* <div>{data.testimonial.text}</div> */}
     </main>
-  )
+  );
 }
